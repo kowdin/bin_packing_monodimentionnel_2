d56c116268217192881db61f565afeb1c02554e1
smbct <buchet.s@laposte.net>
2017-05-29 00:37:52 +0200

    test script

--- code/bin_packing.py
+++ code/bin_packing.py
@@ -6,20 +6,23 @@ from heuristique import best_fit
 from lagrange import relax_lagrange
 from reparation import *
 
-from random import randint
-import math
-
 from lagrange_kp2 import relax_lagrange_kp
 
-inst = Instance("../Instances/c150/Falkenauer_u120_00.txt")
+from random import randint
+
+import math
+import sys
+
+
+# inst = Instance("../Instances/c150/Falkenauer_u120_00.txt")
 #inst = Instance("../Instances/c1000/Falkenauer_t120_03.txt")
 
-# inst = Instance("jouet2.txt")
+inst = Instance(sys.argv[1])
 
 #inst.afficher()
 
-print("L'heuristique best fit donne : " + str(best_fit(inst)))
-print("La relaxation lineaire donne : " + str(inst.relaxation_lineaire()) )
+print("best fit : " + str(best_fit(inst)))
+print("relaxation lineaire : " + str(inst.relaxation_lineaire()) )
 
 #(val,x,u,mu,gamma) = relax_lagrange(inst)
 #print("La relaxation lagrangienne 1 donne : " + str(val)+" => "+str(math.ceil(val)))
@@ -38,7 +41,7 @@ print("La relaxation lineaire donne : " + str(inst.relaxation_lineaire()) )
 
 
 (val,x,u,mu) = relax_lagrange_kp(inst)
-print("La relaxation lagrangienne 2 donne : " + str(val)+" => "+str(math.ceil(val)))
-print("\touverture : "+str(u))
-print("\t associations : "+str(x))
-print("Coeficient lagrangien : "+str(mu))
+print("relaxation lagrangienne : " + str(math.ceil(val)))
+# print("\touverture : "+str(u))
+# print("\t associations : "+str(x))
+# print("Coeficient lagrangien : "+str(mu))
